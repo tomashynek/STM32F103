301014f0415bd976dd3fffa057f9a34b1726af62
Tomas Hynek <tomas.hynek@seznam.cz>
2020-04-10 16:24:13 +0200

    initial commit for serial_helloworld project

---
+++ projects/bluepill_serial_helloworld/src/serial_helloworld.c
@@ -0,0 +1,14 @@
+#include "stm32f103xb.h"
+
+int main(){
+	SystemCoreClockUpdate();
+    // TODO: enable clock for serial1
+    // TODO: define char array to be sent
+    // TODO: setup serial transmission
+    // TODO: in while loop repeat over and over
+    
+    // Infinite loop
+	while(1){
+	}
+	return 0;
+}
