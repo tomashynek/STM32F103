9158922f2ece570307032dae93bf33520810c41d
Tomas Hynek <tomas.hynek@seznam.cz>
2021-07-06 20:58:11 +0200

    Finished with python plotting

--- projects/bluepill_adc_external/src/adc_external.c
+++ projects/bluepill_adc_external/src/adc_external.c
@@ -14,6 +14,11 @@ int main(){
     // CNF1 CNF0 Mode1 Mode0  = 1011 = 0xB at pin9
     // Read -> Clear CNF and MODE for Pin9 -> Set 0xB -> Write
     GPIOA -> CRH = ((GPIOA -> CRH & ~(GPIO_CRH_CNF9_Msk | GPIO_CRH_MODE9_Msk)) | (0xB << GPIO_CRH_MODE9_Pos));
+
+    // Set pin PA0 (ADC0) to Analog 
+    // CNF1 CNF0 Mode1 Mode0 = 0000 at pin0
+    GPIOA -> CRL &= ~GPIO_CRL_CNF0_Msk;
+    GPIOA -> CRL &= ~GPIO_CRL_MODE0_Msk;
     
     // Initialise USART1 for transmission
     // Enable USART1
@@ -27,14 +32,14 @@ int main(){
     // Set TE to 1
     USART1 -> CR1 |= USART_CR1_TE;
 
-    // Initialise ADC1 for conversion of internal channels (reference voltage or temperature sensor)
+    // Initialise ADC1 for conversion of external channels
     ADC1 -> CR2 |= ADC_CR2_ADON; // Turn ADC1 ON
     ADC1 -> CR2 |= ADC_CR2_TSVREFE; // Enable internal channels
     ADC1 -> CR2 &= ~ADC_CR2_ALIGN; // Right alignment of the results
     ADC1 -> CR2 &= ~ADC_CR2_CONT; // Single mode
-    ADC1 -> SQR3 = (ADC1 -> SQR3 & ~ADC_SQR3_SQ1_Msk) | (17U << ADC_SQR3_SQ1_Pos); // Channel 17 (V_REF) as 1st input in sequence
+    ADC1 -> SQR3 = (ADC1 -> SQR3 & ~ADC_SQR3_SQ1_Msk) | (0U << ADC_SQR3_SQ1_Pos); // Channel 0 (ADC0 pin PA0 ) as 1st input in sequence
     ADC1 -> SQR1 = (ADC1 -> SQR1 & ~ADC_SQR1_L_Msk) | (0U << ADC_SQR1_L_Pos); // Set number of channels in sequence to 1 (=0000)
-    ADC1 -> SMPR1 = (ADC1 -> SMPR1 & ~ADC_SMPR1_SMP17_Msk) | (7U << ADC_SMPR1_SMP17_Pos); // Set sampling time of channel 17 to 239.5 cycle (~ 21us @ 12MHz)
+    ADC1 -> SMPR2 = (ADC1 -> SMPR2 & ~ADC_SMPR2_SMP0_Msk) | (7U << ADC_SMPR2_SMP0_Pos); // Set sampling time of channel 0 to 239.5 cycle (~ 21us @ 12MHz)
     // Calibration
     ADC1 -> CR2 |= ADC_CR2_CAL; // Start calibration
     while((ADC1 -> CR2 & ADC_CR2_CAL_Msk) != 0){} // Wait until CAL is cleared by HW
--- projects/bluepill_adc_external/tools/pyserial_reader.py
+++ projects/bluepill_adc_external/tools/pyserial_reader.py
@@ -1,10 +1,23 @@
 import serial
+from matplotlib import pyplot as plt
+import numpy
+import time
 
 ser = serial.Serial('/dev/ttyUSB0')
 print(ser.name)
-while(1):
+L = 5000
+results = numpy.ndarray((L,),int)
+i = 0
+while i < L:
     data = ser.readline()
     print(data)
+    if(len(data)) != 3:
+            continue
     reading = data[0]*256 + data[1]
     print("ADC reading =", reading)
-    print("Voltage = ", reading/4096*3.3)
+    print("Voltage = {:.3f}".format(reading/4096*3.3))
+    results[i] = reading
+    i += 1
+    
+plt.plot(results)
+plt.show()
